8a33fcaa94daf65b1cb5d70d5c6ad4fa21c9d255
zhaoyun1996 <kiet14020248uet@gmail.com>
2022-06-05 12:12:21 +0700

    Thêm module image

---
+++ services/mutlerServices.js
@@ -0,0 +1,33 @@
+/**
+ * Multer settings
+ */
+
+const multer = require("multer");
+
+const ALLOWED_FORMATS = ["image/jpeg", "image/png", "image/jpg"];
+
+const storage = multer.memoryStorage();
+
+const upload = multer({
+    storage,
+    fileFilter: function (req, file, cb) {
+        if (ALLOWED_FORMATS.includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+            cb(new Error("Not supported file type"), false);
+        }
+    },
+});
+
+const singleUpload = upload.single("file");
+
+exports.singleUploadCtrl = (req, res, next) => {
+    singleUpload(req, res, (error) => {
+        if (error) {
+            return res.status(422).send({
+                message: "Image upload failed",
+            });
+        }
+        next();
+    });
+};
